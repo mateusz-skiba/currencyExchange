c72655832e32cd0cd5f823d303ea30a4f3de2201
Skibol <mateusz.skiba14@gmail.com>
2020-05-10 15:15:38 +0200

    Updates

---
+++ precache-manifest.2ad390e49a506c93435b7381d919756b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42cbd894f06c8f4dbe8974ef132e161",
+    "url": "/currencyExchange/index.html"
+  },
+  {
+    "revision": "cd37780454f0fcca8471",
+    "url": "/currencyExchange/static/css/main.2cef1cf8.chunk.css"
+  },
+  {
+    "revision": "45c445beacc4cf6aae48",
+    "url": "/currencyExchange/static/js/2.0837a7e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currencyExchange/static/js/2.0837a7e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd37780454f0fcca8471",
+    "url": "/currencyExchange/static/js/main.4ae6cfd3.chunk.js"
+  },
+  {
+    "revision": "6ecca31ad5b638e8f30a",
+    "url": "/currencyExchange/static/js/runtime-main.efc6963a.js"
+  }
+]);
